33a0f0dd5b4c75384e716f12b972fad598f462d7
Angeliki Milonaki <angmilonaki@gmail.com>
2018-04-21 16:47:37 +0300

    Adding more structure

--- Chord.py
+++ Chord.py
@@ -1,5 +1,10 @@
 import argparse
 
+def findMaxNodes():
+
+
+
+
 def generateRandomIpsAndPorts(N):
     #given the number of nodes needed for the DS
     #return a list of [nodeId, ip, port]
@@ -8,35 +13,46 @@ def generateRandomIpsAndPorts(N):
     #nodeId is the result when hashing id+port (concatenation)
     #Should also check that the random ips are unique
 
+    NodeList=[]
+    findClosestPower(N)
+
+
 
 class node(object):
     def init(self,id):
         nodeId=id
         inQueue=[]
+        fingerTable=[]
 
 
     def isFileStoredLocally(requestId):
         #this method should check if the current node contains the
         #file asked
         #returns true,false
+        pass
 
     def readFromQueue():
         #When it is the turn for the node to handle (send and receive) requests
         #this function should check what is present in the nodes incoming Queue( the queue containing the
         #requests that were written from others in the nodes "shared memory"
+        pass
 
-   def requestFile():
-       #this function will be used after a node has:
-       #---received a request
-       #---searched it has the file requested
-       #---if the file is not stored loccaly
-       #    ---find the most suitable node to pass the request
-       #    ---send the request to the node found above
+    def requestFile():
+        #this function will be used after a node has:
+        #---received a request
+        #---searched it has the file requested
+        #---if the file is not stored loccaly
+        #    ---find the most suitable node to pass the request
+        #    ---send the request to the node found above
+        pass
 
-   def findNextNode(f):
-       #given the requested file (id), find the best node to pass the request
-       #if file is not stored locally
+    def findNextNode(f):
+        #given the requested file (id), find the best node to pass the request
+        #if file is not stored locally
+        pass
 
+    def updateFingerTable():
+        pass
 
 
 #executing script using --->python Chord.py --N <number>
